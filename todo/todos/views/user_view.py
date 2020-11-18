9a60782301a6bd6b2d546d4d6112ec5642e10ca7
Hadi Pirhadi <hphadi@hotmail.com>
2020-11-18 09:46:03 +0100

    Add permission

--- todo/todos/views/user_view.py
+++ todo/todos/views/user_view.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 from ..serializers import UserSerializer
 
@@ -7,3 +7,4 @@ from ..serializers import UserSerializer
 class UserView(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [permissions.IsAdminUser]
